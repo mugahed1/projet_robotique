11ca5729278154495b8099022f994b7544ebe3be
mugahed1 <mugahed.abdulla@epfl.ch>
2022-04-21 16:24:33 +0200

    commit main

---
+++ audio_processing.c
@@ -0,0 +1,153 @@
+#include "ch.h"
+#include "hal.h"
+#include <main.h>
+#include <usbcfg.h>
+#include <chprintf.h>
+
+#include <motors.h>
+#include <audio/microphone.h>
+#include <audio_processing.h>
+#include <fft.h>
+#include <arm_math.h>
+
+//semaphore
+static BSEMAPHORE_DECL(sendToComputer_sem, TRUE);
+
+//2 times FFT_SIZE because these arrays contain complex numbers (real + imaginary)
+static float micLeft_cmplx_input[2 * FFT_SIZE];
+static float micRight_cmplx_input[2 * FFT_SIZE];
+
+//Arrays containing the computed magnitude of the complex numbers
+static float micLeft_output[FFT_SIZE];
+static float micRight_output[FFT_SIZE];
+static float micFront_output[FFT_SIZE];
+static float micBack_output[FFT_SIZE];
+
+#define MIN_VALUE_THRESHOLD	10000 
+
+#define MIN_FREQ		10	//we don't analyze before this index to not use resources for nothing
+#define FREQ_FORWARD	16	//250Hz
+#define MAX_FREQ		30	//we don't analyze after this index to not use resources for nothing
+
+
+/*
+*	Simple function used to detect the highest value in a buffer
+*	and to execute a motor command depending on it
+*/
+
+/*
+*	Callback called when the demodulation of the four microphones is done.
+*	We get 160 samples per mic every 10ms (16kHz)
+*	
+*	params :
+*	int16_t *data			Buffer containing 4 times 160 samples. the samples are sorted by micro
+*							so we have [micRight1, micLeft1, micBack1, micFront1, micRight2, etc...]
+*	uint16_t num_samples	Tells how many data we get in total (should always be 640)
+*/
+void processAudioData(int16_t *data, uint16_t num_samples){
+
+	/*
+	*
+	*	We get 160 samples per mic every 10ms
+	*	So we fill the samples buffers to reach
+	*	1024 samples, then we compute the FFTs.
+	*
+	*/
+
+	static uint16_t nb_samples = 0;
+	static uint8_t mustSend = 0;
+
+	//loop to fill the buffers
+	for(uint16_t i = 0 ; i < num_samples ; i+=4){
+		//construct an array of complex numbers. Put 0 to the imaginary part
+		micRight_cmplx_input[nb_samples] = (float)data[i + MIC_RIGHT];
+		micLeft_cmplx_input[nb_samples] = (float)data[i + MIC_LEFT];
+		micBack_cmplx_input[nb_samples] = (float)data[i + MIC_BACK];
+		micFront_cmplx_input[nb_samples] = (float)data[i + MIC_FRONT];
+
+		nb_samples++;
+
+		micRight_cmplx_input[nb_samples] = 0;
+		micLeft_cmplx_input[nb_samples] = 0;
+		micBack_cmplx_input[nb_samples] = 0;
+		micFront_cmplx_input[nb_samples] = 0;
+
+		nb_samples++;
+
+		//stop when buffer is full
+		if(nb_samples >= (2 * FFT_SIZE)){
+			break;
+		}
+	}
+
+	if(nb_samples >= (2 * FFT_SIZE)){
+		/*	FFT proccessing
+		*
+		*	This FFT function stores the results in the input buffer given.
+		*	This is an "In Place" function. 
+		*/
+
+		doFFT_optimized(FFT_SIZE, micRight_cmplx_input);
+		doFFT_optimized(FFT_SIZE, micLeft_cmplx_input);
+		doFFT_optimized(FFT_SIZE, micFront_cmplx_input);
+		doFFT_optimized(FFT_SIZE, micBack_cmplx_input);
+
+		/*	Magnitude processing
+		*
+		*	Computes the magnitude of the complex numbers and
+		*	stores them in a buffer of FFT_SIZE because it only contains
+		*	real numbers.
+		*
+		*/
+		arm_cmplx_mag_f32(micRight_cmplx_input, micRight_output, FFT_SIZE);
+		arm_cmplx_mag_f32(micLeft_cmplx_input, micLeft_output, FFT_SIZE);
+		arm_cmplx_mag_f32(micFront_cmplx_input, micFront_output, FFT_SIZE);
+		arm_cmplx_mag_f32(micBack_cmplx_input, micBack_output, FFT_SIZE);
+
+		//sends only one FFT result over 10 for 1 mic to not flood the computer
+		//sends to UART3
+		if(mustSend > 8){
+			//signals to send the result to the computer
+			chBSemSignal(&sendToComputer_sem);
+			mustSend = 0;
+		}
+		nb_samples = 0;
+		mustSend++;
+
+		sound_remote(micLeft_output);
+	}
+}
+
+void wait_send_to_computer(void){
+	chBSemWait(&sendToComputer_sem);
+}
+
+float* get_audio_buffer_ptr(BUFFER_NAME_t name){
+	if(name == LEFT_CMPLX_INPUT){
+		return micLeft_cmplx_input;
+	}
+	else if (name == RIGHT_CMPLX_INPUT){
+		return micRight_cmplx_input;
+	}
+	else if (name == FRONT_CMPLX_INPUT){
+		return micFront_cmplx_input;
+	}
+	else if (name == BACK_CMPLX_INPUT){
+		return micBack_cmplx_input;
+	}
+	else if (name == LEFT_OUTPUT){
+		return micLeft_output;
+	}
+	else if (name == RIGHT_OUTPUT){
+		return micRight_output;
+	}
+	else if (name == FRONT_OUTPUT){
+		return micFront_output;
+	}
+	else if (name == BACK_OUTPUT){
+		return micBack_output;
+	}
+	else{
+		return NULL;
+	}
+}
---
+++ main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ch.h"
+#include "hal.h"
+#include "memory_protection.h"
+#include <usbcfg.h>
+#include <main.h>
+#include <chprintf.h>
+#include <motors.h>
+#include <audio/microphone.h>
+
+#include <audio_processing.h>
+#include <fft.h>
+#include <arm_math.h>
+
+//uncomment to send the FFTs results from the real microphones
+#define SEND_FROM_MIC
+
+
+
+
+
+int main(void)
+{
+    halInit();
+    chSysInit();
+    mpu_init();
+    motors_init();
+
+
+
+#ifdef SEND_FROM_MIC
+    //starts the microphones processing thread.
+    //it calls the callback given in parameter when samples are ready
+    mic_start(&processAudioData);
+#endif  /* SEND_FROM_MIC */
+
+    /* Infinite loop. */
+    while (1)
+    {
+    	asm  ("nop");
+
+     }
+
+}
+#define STACK_CHK_GUARD 0xe2dee396
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+void __stack_chk_fail(void)
+{
+    chSysHalt("Stack smashing detected");
+}
