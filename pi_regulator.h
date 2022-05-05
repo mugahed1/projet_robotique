bb91d72205a91effc2859d6942bdd7e905c30043
Leabli <48591915+Leabli@users.noreply.github.com>
2022-05-05 11:54:21 +0200

    PI regulator integration

---
+++ pi_regulator.h
@@ -0,0 +1,14 @@
+/*
+ * pi_regulator.h
+ *
+ *  Created on: 22 avr. 2022
+ *      Author: leabliniere
+ */
+
+#ifndef PI_REGULATOR_H_
+#define PI_REGULATOR_H_
+
+//start the PI regulator thread
+void pi_regulator_start(void);
+
+#endif /* PI_REGULATOR_H_ */
